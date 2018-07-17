60ebe21eca66898a8a6bc553cac1e0b9b0ed7dc4
Toufeeq umer <b4u2umer@gmail.com>
2018-07-17 15:17:47 +0530

    initialized b9eng
    initialized b9eng with components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+ import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ServicesComponent } from './services/services.component';
+import { ProductComponent } from './product/product.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './home/header/header.component';
+import { SectionComponent } from './home/section/section.component';
+import { FooterComponent } from './home/footer/footer.component';
 
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ServicesComponent,
+    ProductComponent,
+    ContactComponent,
+    HeaderComponent,
+    SectionComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    RouterModule.forRoot(appRoutes) ],
   providers: [],
   bootstrap: [AppComponent]
 })
